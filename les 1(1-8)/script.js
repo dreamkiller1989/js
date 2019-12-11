a76bb76fe75247ba0f3318c368fcabf163e471af
dreamkiller1989 <57640845+dreamkiller1989@users.noreply.github.com>
2019-12-11 16:55:08 +0200

    Add files via upload

---
+++ les 1(1-8)/script.js
@@ -0,0 +1,79 @@
+//1//
+let str = 'Привет';
+let num = 123;
+let flag = true;
+let txt = 'true';
+console. log(str);
+console. log(num);
+console. log(flag);
+console. log(txt);
+console. log(typeof'str');
+console. log(typeof'num');
+console. log(typeof'flag');
+console. log(typeof'txt');
+//#1//
+//2//
+let a1 = 5+3;
+let a2 = 5-3;
+let a3 = 5*3;
+let a4 = 5/3;
+console. log(a1);
+console. log(a2);
+console. log(a3);
+console. log(a4);
+//#2//
+//3//
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 +'3';
+let a9 = '5'-3;
+let a10 = 75 + 'кг';
+console. log(a6);
+console. log(a7);
+console. log(a8);
+console. log(a9);
+console. log(a10);
+//#3//
+//4//
+let height = 23;
+let width = 10;
+let s = height*width+'см';
+console. log(height);
+console. log(width);
+console. log(s);
+//#4//
+//5//
+let heightC = 10;
+let dC = 4;
+let rC = dC/2;
+let S = rC**2*3.14;
+let V = S*heightC+'м';
+console. log(heightC);
+console. log(dC) ;
+console. log(rC);
+console. log(S);
+console. log(V);
+//#5//
+//6//
+let n = 3;
+let m = 4;
+let k2 = n**2+m**2;
+let k = Math.sqrt (k2);
+console. log(n);
+console. log(m);
+console. log(k2);
+console.log(k);
+//#6//
+//7//
+let str1 = '"Hello world"';
+console.log(str1);
+document.write(str1);
+alert(str1)
+//#7//
+//8//
+let fio ='Березанский Максим Сергеевич\n 30 лет\n автолюбитель'
+alert(fio)
+//#8//
+
+
+
---
+++ les 1(9-14)/script1.js
@@ -0,0 +1,8 @@
+//9//
+let str4 = 'кто'
+let str2 = 'ты'
+let str3 = 'такой'
+let concatenation = str4 +str2 +str3
+console.log(concatenation)
+document.write(concatenation)
+
